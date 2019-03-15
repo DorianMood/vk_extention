5493f4433483bab7a3a14c38d1ab8815b6d0ff5e
nekithockey75 <nikita01051997@gmail.com>
2019-03-15 20:49:52 +0500

    Added hotkeys and buttons

--- content_script.js
+++ content_script.js
@@ -1,53 +1,139 @@
+let ENABLE_ADDING = true;
+
 $(document).ready(() => {
 
-	$('.page_actions_wide').after('<div class=\"page_actions_wide clear_fix\">  <div class=\"page_action_left fl_l\" id=\"friend_status\">    <div class=\"profile_action_btn\"><button class=\"flat_button button_wide\" id=\"add-to-cart\">Добавить в корзину</button></div>  </div></div>');
-	// $('.profile_actions_wide').after('<div class=\"page_actions_wide clear_fix\">  <div class=\"page_action_left fl_l\" id=\"friend_status\">    <div class=\"profile_action_btn\"><button class=\"flat_button button_wide\">Добавить в корзину</button></div>  </div></div>');
 
-	$('#add-to-cart').on('click', () => {
-		console.log('adding');
+	/*
+	Обработка списка друзей
+	*/
+	console.log($('.friends_list_bl').children().css('background-color', 'red'));
 
-		let person = {
-			link: window.location.href,
-			name: $(".page_name")[0].innerText
-		};
-		chrome.extension.sendMessage({type: "add_true", person: person}, (response) => {
-			console.log(`Response on method add_true : ${response}`);
-		});
-	});
 
-	chrome.storage.local.get(function(result){console.log(result)});
 
-	$(document).keydown((e) => {
-		if (e.ctrlKey && String.fromCharCode(e.which).toLowerCase() === 'e') {
-			e.preventDefault();
-
+	/*
+	Добавление кнопки добавить в корзину.
+	Работа с событиями для коррректной работы этой кнопки.
+	*/
+	if ($('.add-to-cart').length === 0) {
+		$('.page_actions_wide').after('<div class=\"page_actions_wide clear_fix\">  <div class=\"page_action_left fl_l\" id=\"friend_status\">    <div class=\"profile_action_btn\"><button class=\"flat_button button_wide add-to-cart\">Добавить в корзину</button></div>  </div></div>');
+			// $('.profile_actions_wide').after('<div class=\"page_actions_wide clear_fix\">  <div class=\"page_action_left fl_l\" id=\"friend_status\">    <div class=\"profile_action_btn\"><button class=\"flat_button button_wide\">Добавить в корзину</button></div>  </div></div>');
+		ENABLE_ADDING = true;
+		$('.add-to-cart').on('click', () => {
+			console.log('adding');
 			let person = {
-				link: $("._im_page_peer_name")[0].href,
-				name: $("._im_page_peer_name")[0].innerText
+				link: window.location.href,
+				name: $(".page_name")[0].innerText
 			};
 			chrome.extension.sendMessage({type: "add_true", person: person}, (response) => {
 				console.log(`Response on method add_true : ${response}`);
 			});
-			chrome.storage.local.get(function(result){console.log(result)});
-		}
-		if (e.ctrlKey && String.fromCharCode(e.which).toLowerCase() === 'q') {
-			e.preventDefault();
-			let person = {
-				name: $("._im_page_peer_name")[0].href,
-				link: $("._im_page_peer_name")[0].innerText
-			};
-			chrome.extension.sendMessage({type: "add_not_true", person: person}, (response) => {
-				console.log(response);
-			});
-			chrome.storage.local.get(function(result){console.log(result)});
-		}
-		if (e.ctrlKey && String.fromCharCode(e.which).toLowerCase() === 'd') {
-			e.preventDefault();
-			chrome.extension.sendMessage({type: "clear"}, (response) => {
-				console.log(response)
-			});
+			$('.add-to-cart').addClass('secondary').css('pointer-events', 'none');
+			ENABLE_ADDING = false;
+		});
+	}
 
-			chrome.storage.local.get(function(result){console.log(result)});
+	$('.page_name').on('DOMSubtreeModified', () => {
+		console.log('123');
+	});
+
+	$('#utils').on('DOMSubtreeModified', () => {
+		if ($('.add-to-cart').length === 0) {
+			$('.page_actions_wide').after('<div class=\"page_actions_wide clear_fix\">  <div class=\"page_action_left fl_l\" id=\"friend_status\">    <div class=\"profile_action_btn\"><button class=\"flat_button button_wide add-to-cart\">Добавить в корзину</button></div>  </div></div>');
+			// $('.profile_actions_wide').after('<div class=\"page_actions_wide clear_fix\">  <div class=\"page_action_left fl_l\" id=\"friend_status\">    <div class=\"profile_action_btn\"><button class=\"flat_button button_wide\">Добавить в корзину</button></div>  </div></div>');
+			ENABLE_ADDING = true;
+			$('.add-to-cart').on('click', () => {
+				console.log('adding');
+
+				let person = {
+					link: window.location.href,
+					name: $(".page_name")[0].innerText
+				};
+				chrome.extension.sendMessage({type: "add_true", person: person}, (response) => {
+					console.log(`Response on method add_true : ${response}`);
+				});
+
+				$('.add-to-cart').addClass('secondary').css('pointer-events', 'none');
+				ENABLE_ADDING = false;
+			});
 		}
 	});
-});
+
+
+	/*
+	Обработка событий горячих клавиш.
+	*/
+		$(document).keydown((e) => {
+			if (e.ctrlKey && String.fromCharCode(e.which).toLowerCase() === 'e') {
+				e.preventDefault();
+			}
+			if ($('.page_name').length > 0) {
+				if (e.ctrlKey && String.fromCharCode(e.which).toLowerCase() === 'e' && ENABLE_ADDING) {
+					e.preventDefault();
+
+					let person = {
+						link: window.location.href,
+						name: $(".page_name")[0].innerText
+					};
+					chrome.extension.sendMessage({type: "add_true", person: person}, (response) => {
+						console.log(`Response on method add_true : ${response}`);
+					});
+					chrome.storage.local.get(function(result){console.log(result)});
+					$('.add-to-cart').addClass('secondary').css('pointer-events', 'none');
+					ENABLE_ADDING = false;
+				}
+				if (e.ctrlKey && String.fromCharCode(e.which).toLowerCase() === 'q') {
+					e.preventDefault();
+					let person = {
+						link: window.location.href,
+						name: $(".page_name")[0].innerText
+					};
+					chrome.extension.sendMessage({type: "add_not_true", person: person}, (response) => {
+						console.log(response);
+					});
+					chrome.storage.local.get(function(result){console.log(result)});
+				}
+				if (e.ctrlKey && String.fromCharCode(e.which).toLowerCase() === 'd') {
+					e.preventDefault();
+					chrome.extension.sendMessage({type: "clear"}, (response) => {
+						console.log(response)
+					});
+
+					chrome.storage.local.get(function(result){console.log(result)});
+				}
+			} else if ($('._im_page_peer_name').length > 0) {
+				if (e.ctrlKey && String.fromCharCode(e.which).toLowerCase() === 'e' && ENABLE_ADDING) {
+					e.preventDefault();
+
+					let person = {
+						link: $("._im_page_peer_name")[0].href,
+						name: $("._im_page_peer_name")[0].innerText
+					};
+					chrome.extension.sendMessage({type: "add_true", person: person}, (response) => {
+						console.log(`Response on method add_true : ${response}`);
+					});
+					chrome.storage.local.get(function(result){console.log(result)});
+					$('.add-to-cart').addClass('secondary').css('pointer-events', 'none');
+					ENABLE_ADDING = false;
+				}
+				if (e.ctrlKey && String.fromCharCode(e.which).toLowerCase() === 'q') {
+					e.preventDefault();
+					let person = {
+						name: $("._im_page_peer_name")[0].href,
+						link: $("._im_page_peer_name")[0].innerText
+					};
+					chrome.extension.sendMessage({type: "add_not_true", person: person}, (response) => {
+						console.log(response);
+					});
+					chrome.storage.local.get(function(result){console.log(result)});
+				}
+				if (e.ctrlKey && String.fromCharCode(e.which).toLowerCase() === 'd') {
+					e.preventDefault();
+					chrome.extension.sendMessage({type: "clear"}, (response) => {
+						console.log(response)
+					});
+
+					chrome.storage.local.get(function(result){console.log(result)});
+				}
+			}
+		});
+	});
